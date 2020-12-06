af79f2fd8ef43a03abf0976bd34b83b61f286d11
prem <prem.sherpa@codeimmersives.com>
2020-12-06 15:56:51 -0500

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import jwtDecode from "jwt-decode";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Signup from "./signup/Signup";
+import Appointment from "./appointment/Appointment";
+import PrivateRoute from "./shared/PrivateRoute";
+
+export default class App extends Component {
+  state = {
+    isAuth: false,
+    user: null,
+  };
+
+  componentDidMount() {
+    let token = localStorage.getItem("jwtToken");
+    console.log('app.js hi')
+
+    if (token) {
+      let decoded = jwtDecode(token);
+
+      this.setState({
+        isAuth: true,
+        user: {
+          email: decoded.email,
+        },
+      });
+    }
+  }
+
+  auth = () => {
+    // let decoded = jwtDecode(jwtToken);
+
+    this.setState({
+      isAuth: true,
+      // user: {
+      //   email: decoded.email,
+      //   _id: decoded._id,
+      // },
+    });
+  };
+
+  logout = () => {
+    localStorage.removeItem("jwtToken");
+    this.setState({
+      isAuth: false,
+      user: null,
+    });
+  };
+
+  render() {
+    console.clear();
+    console.log(this.props);
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              component={(props) => <Signup {...props} auth={this.auth} logout={this.logout}/>}
+            />
+            
+            <PrivateRoute
+              exact
+              path="/make-appointment"
+              component={Appointment}
+              isAuth={this.state.isAuth}
+              logout={this.logout}
+            />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
-
-export default App;
